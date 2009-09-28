afc7d734b59f54926d53c8149021bf88a7cefec6
jonas@halfdanj.dk <jonas@halfdanj.dk@136808a2-995b-11de-a465-5fc2cdd6756b>
2009-09-28 09:49:36 +0000

    intial commit - camera calibrator app for getting the camera intrisics

---
+++ frost/cameraCalibrator/src/testApp.h
@@ -0,0 +1,41 @@
+/*
+*  camera calibration
+*  stefanix - helloworld@stefnix.net
+*/
+
+#include <iostream>
+#include <vector>
+
+
+#include "ofMain.h"
+#include "ofxCvMain.h"
+#include "ofCvCameraCalibration.h"
+#include "ofxVectorMath.h"
+
+
+
+class testApp : public ofSimpleApp {
+  public:
+	void setup();
+	void update();
+	void draw();
+	void keyPressed( int key );
+	void mouseMoved( int x, int y );
+	void mouseDragged( int x, int y, int button );
+	void mousePressed( int x, int y, int button );
+	void mouseReleased();
+    void drawImage( IplImage* image, int x, int y, int width, int height );
+    
+    int cwidth;
+    int cheight;
+    CvSize csize;
+	ofVideoGrabber  vidGrabber;
+    ofxCvColorImage  colorImg;
+    ofxCvGrayscaleImage  undistortedImg;
+
+    ofCvCameraCalibration calib;
+    int shotToShow;
+    bool bUndistortLiveFeed;
+
+};
+
